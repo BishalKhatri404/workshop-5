ab452d2365cf2a5bbeaa15fb62bae255cc7f4e17
BishalKhatri404 <khatrybishal20@gmail.com>
2025-11-26 11:48:06 +0545

    task 3 completed

---
+++ task3/Printer.cs
@@ -0,0 +1,23 @@
+﻿public class Printer
+{
+    // 1. Print string message
+    public void Print(string message)
+    {
+        Console.WriteLine("Message: " + message);
+    }
+
+    // 2. Print integer number
+    public void Print(int number)
+    {
+        Console.WriteLine("Number: " + number);
+    }
+
+    // 3. Print message multiple times
+    public void Print(string message, int count)
+    {
+        for (int i = 1; i <= count; i++)
+        {
+            Console.WriteLine(i + ". " + message);
+        }
+    }
+}
---
+++ task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Printer p = new Printer();
+
+        // Call Print(string)
+        p.Print("Hello World");
+
+        // Call Print(int)
+        p.Print(123);
+
+        // Call Print(string, int)
+        p.Print("Repeat Message", 3);
+
+        Console.ReadLine();
+    }
+}
