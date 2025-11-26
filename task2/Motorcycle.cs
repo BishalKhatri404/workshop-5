447f8d3c1c8eb8e83b9095e90bc2e348c454ceae
BishalKhatri404 <khatrybishal20@gmail.com>
2025-11-26 11:42:10 +0545

    task2 completed

---
+++ task2/Motorcycle.cs
@@ -0,0 +1,10 @@
+﻿public class Motorcycle : Vehicle
+{
+    public bool HasCarrier { get; set; }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine("Has Carrier: " + (HasCarrier ? "Yes" : "No"));
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // --- Car Object ---
+        Car car = new Car();
+        car.Brand = "Toyota";
+        car.Speed = 180;
+        car.Seats = 5;
+
+        Console.WriteLine("---- Car Info ----");
+        car.Start();
+        car.DisplayInfo();
+        car.Stop();
+
+        Console.WriteLine();
+
+        // --- Motorcycle Object ---
+        Motorcycle moto = new Motorcycle();
+        moto.Brand = "Yamaha";
+        moto.Speed = 120;
+        moto.HasCarrier = true;
+
+        Console.WriteLine("---- Motorcycle Info ----");
+        moto.Start();
+        moto.DisplayInfo();
+        moto.Stop();
+
+        Console.ReadLine();
+    }
+}
